17472aadb200a84e85e794f3a3a69bd26c191d20
Abdul Ragib <abdulragib36@gmail.com>
2023-02-11 20:36:15 +0530

    Solution 2.0

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
-import calender from './calender'
+import Calender from './calender.js'
 
 const App = () => {
   return (
     <div id="main">
-      <calender/>
+      <Calender/>
     </div>
   )
 }
